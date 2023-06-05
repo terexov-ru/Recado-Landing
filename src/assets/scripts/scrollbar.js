13c3e1582aced4daf58cb40efbcce2ba7894d6e5
RGABear <russiangamebear@mail.ru>
2023-06-06 00:05:56 +0300

    improved slider on mobile

--- src/assets/scripts/scrollbar.js
+++ src/assets/scripts/scrollbar.js
@@ -1,66 +1,42 @@
 import Swiper from "swiper/bundle";
 document.addEventListener("DOMContentLoaded", function () {
-  if ($(window).width() > 1400) {
-    const swiper = new Swiper(".mySwiper", {
-      direction: "horizontal",
-      scrollbar: {
-        el: ".swiper-scrollbar",
-        draggable: true,
-        dragSize: "auto",
-        dragClass: "swiper-scrollbar-drag",
-        horizontalClass: "swiper-scrollbar-horizontal",
-        hide: false,
+  const swiper = new Swiper(".mySwiper", {
+    direction: "horizontal",
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true,
+      dragSize: "auto",
+      dragClass: "swiper-scrollbar-drag",
+      horizontalClass: "swiper-scrollbar-horizontal",
+      hide: false,
+    },
+    slidesPerView: 4,
+    spaceBetween: 32,
+    breakpoints: {
+      310: {
+        slidesPerView: "auto",
+        spaceBetween: 16,
+        freeMode: {
+          enabled: true,
+          sticky: false,
+        },
       },
-      slidesPerView: 4,
-      spaceBetween: 32,
-    });
-  }
-
-  if ($(window).width() > 1200 && $(window).width() < 1400) {
-    const swiper = new Swiper(".mySwiper", {
-      direction: "horizontal",
-      scrollbar: {
-        el: ".swiper-scrollbar",
-        draggable: true,
-        dragSize: "auto",
-        dragClass: "swiper-scrollbar-drag",
-        horizontalClass: "swiper-scrollbar-horizontal",
-        hide: false,
+      600: {
+        slidesPerView: 2,
+        spaceBetween: 16,
       },
-      slidesPerView: 4,
-      spaceBetween: 16,
-    });
-  }
-
-  if ($(window).width() > 800 && $(window).width() < 1200) {
-    const swiper = new Swiper(".mySwiper", {
-      direction: "horizontal",
-      scrollbar: {
-        el: ".swiper-scrollbar",
-        draggable: true,
-        dragSize: "auto",
-        dragClass: "swiper-scrollbar-drag",
-        horizontalClass: "swiper-scrollbar-horizontal",
-        hide: false,
+      800: {
+        slidesPerView: 3,
+        spaceBetween: 16,
       },
-      slidesPerView: 3,
-      spaceBetween: 16,
-    });
-  }
-
-  if ($(window).width() > 600 && $(window).width() < 800) {
-    const swiper = new Swiper(".mySwiper", {
-      direction: "horizontal",
-      scrollbar: {
-        el: ".swiper-scrollbar",
-        draggable: true,
-        dragSize: "auto",
-        dragClass: "swiper-scrollbar-drag",
-        horizontalClass: "swiper-scrollbar-horizontal",
-        hide: false,
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 16,
       },
-      slidesPerView: 2,
-      spaceBetween: 16,
-    });
-  }
+      1400: {
+        slidesPerView: 4,
+        spaceBetween: 32,
+      },
+    },
+  });
 });
